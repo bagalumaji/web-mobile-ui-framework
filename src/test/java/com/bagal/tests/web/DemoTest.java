5d4ac6b8b8a2756392bae87e6bc9038805912589
Umaji Bagal <bagalumaji@gmail.com>
2024-09-11 09:28:57 +0530

    added test for firefox

--- src/test/java/com/bagal/tests/web/DemoTest.java
+++ src/test/java/com/bagal/tests/web/DemoTest.java
@@ -1,19 +1,32 @@
 package com.bagal.tests.web;
 
+import com.bagal.configs.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class DemoTest {
     @Test
     public void demoTest(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://the-internet.herokuapp.com/login");
-        String title = driver.getTitle();
-        System.out.println("title = " + title);
-        String currentUrl = driver.getCurrentUrl();
-        System.out.println("currentUrl = " + currentUrl);
-        driver.quit();
+        if(ConfigReader.getConfig().browser().equalsIgnoreCase("chrome")) {
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://the-internet.herokuapp.com/login");
+            String title = driver.getTitle();
+            System.out.println("title = " + title);
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("currentUrl = " + currentUrl);
+            driver.quit();
+        }else{
+            WebDriver driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+            driver.get("https://the-internet.herokuapp.com/login");
+            String title = driver.getTitle();
+            System.out.println("title = " + title);
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("currentUrl = " + currentUrl);
+            driver.quit();
+        }
     }
 }
