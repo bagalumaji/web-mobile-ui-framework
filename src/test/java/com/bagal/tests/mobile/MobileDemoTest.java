119fb4cdb8be9af0170cca9c0ec6682cf92f1de9
Umaji Bagal <bagalumaji@gmail.com>
2024-09-22 15:06:53 +0530

    added ios test

--- src/test/java/com/bagal/tests/mobile/MobileDemoTest.java
+++ src/test/java/com/bagal/tests/mobile/MobileDemoTest.java
@@ -1,11 +1,41 @@
 package com.bagal.tests.mobile;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public final class MobileDemoTest {
 
     @Test
-    void launchAndroidTest(){
+    void launchAndroidTest() {
+        try {
+            UiAutomator2Options options = new UiAutomator2Options();
+            options.setDeviceName("emulator-5554");
+            options.setPlatformName("Android");
+            options.setAutomationName("UiAutomator2");
+            options.setApp(System.getProperty("user.dir") + "/src/test/resources/apps/mda-2.1.0-24.apk");
+            AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+            System.out.println("driver.getDeviceTime() = " + driver.getDeviceTime());
+            System.out.println("driver.getContext() = " + driver.getContext());
+            System.out.println("driver.getCapabilities().asMap() = " + driver.getCapabilities().asMap());
+            driver.quit();
+        } catch (Exception e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
+        }
+    }
 
+    @Test
+    void launchIosTest() throws MalformedURLException, InterruptedException {
+        XCUITestOptions options = new XCUITestOptions();
+        options.setDeviceName("iPhone 16");
+        options.setApp(System.getProperty("user.dir") + "/src/test/resources/apps/SauceLabs-Demo-App.Simulator.XCUITest.zip");
+        IOSDriver driver = new IOSDriver(new URL("http://127.0.0.1:4723"), options);
+        Thread.sleep(10000);
+        driver.quit();
     }
 }
