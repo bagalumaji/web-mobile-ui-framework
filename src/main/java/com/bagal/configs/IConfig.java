020346a9a2112d2f274102ad5177b819728b0ba4
Umaji Bagal <bagalumaji@gmail.com>
2024-09-23 07:55:13 +0530

    updated IConfig with new abstract methods

--- src/main/java/com/bagal/configs/IConfig.java
+++ src/main/java/com/bagal/configs/IConfig.java
@@ -1,13 +1,22 @@
 package com.bagal.configs;
 
+import com.bagal.RunModeTypes;
+import com.bagal.convertors.StringToRunModeTypeConvertor;
 import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "file://${user.dir}/src/test/resources/configs/config.properties"
+        "system:properties",
+        "system:env",
+        "file://${user.dir}/src/test/resources/config.properties",
+        "file://${user.dir}/src/test/resources/stag-config.properties"
 })
 public interface IConfig extends Config {
     @Key("browser")
     @DefaultValue("chrome")
     String browser();
+
+    @ConverterClass(value = StringToRunModeTypeConvertor.class)
+    @DefaultValue(value = "web")
+    RunModeTypes runMode();
 }
