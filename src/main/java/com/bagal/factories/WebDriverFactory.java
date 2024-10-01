ff2a746c1f9b016693035d7520b4ac3f0b03f1da
umaji <bagalumaji@gmail.com>
2024-10-01 21:55:58 +0530

    added code to get the chrome driver

--- src/main/java/com/bagal/factories/WebDriverFactory.java
+++ src/main/java/com/bagal/factories/WebDriverFactory.java
@@ -1,5 +1,11 @@
 package com.bagal.factories;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public final class WebDriverFactory {
     private WebDriverFactory() {}
+    public static WebDriver getWebDriver() {
+        return new ChromeDriver();
+    }
 }
