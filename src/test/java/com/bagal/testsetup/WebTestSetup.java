43c800067e58dd07b545294dd3dfb2801d996cbb
umaji <bagalumaji@gmail.com>
2024-10-01 21:56:28 +0530

    refactored name of wed driver to driver

--- src/test/java/com/bagal/testsetup/WebTestSetup.java
+++ src/test/java/com/bagal/testsetup/WebTestSetup.java
@@ -1,16 +1,16 @@
 package com.bagal.testsetup;
 
-import com.bagal.driver.web.WebDriver;
+import com.bagal.driver.web.Driver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class WebTestSetup {
     @BeforeMethod
     public void setup(){
-        WebDriver.initDriver();
+        Driver.initDriver();
     }
     @AfterMethod
     public void teardown(){
-        WebDriver.closeDriver();
+        Driver.closeDriver();
     }
 }
