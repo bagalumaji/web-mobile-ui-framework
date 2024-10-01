4eeb67907afccddf763e440d277a216cb02b5c82
umaji <bagalumaji@gmail.com>
2024-10-01 21:55:17 +0530

    code for driver manager

--- src/main/java/com/bagal/driver/manager/DriverManager.java
+++ src/main/java/com/bagal/driver/manager/DriverManager.java
@@ -1,5 +1,22 @@
 package com.bagal.driver.manager;
 
+import org.openqa.selenium.WebDriver;
+
 public final class DriverManager {
-    private DriverManager() {}
+    private DriverManager() {
+    }
+
+    private final static ThreadLocal<WebDriver> WEB_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static WebDriver getWebDriver() {
+        return WEB_DRIVER_THREAD_LOCAL.get();
+    }
+
+    public static void setWebDriver(WebDriver driver) {
+        WEB_DRIVER_THREAD_LOCAL.set(driver);
+    }
+
+    public static void unload() {
+        WEB_DRIVER_THREAD_LOCAL.remove();
+    }
 }
