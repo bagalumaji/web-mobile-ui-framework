2b3773117d5d59a080d515aa1b4dfb932d235c33
umaji <bagalumaji@gmail.com>
2024-10-01 22:10:27 +0530

    updated web driver factory class

--- src/main/java/com/bagal/factories/WebDriverFactory.java
+++ src/main/java/com/bagal/factories/WebDriverFactory.java
@@ -1,11 +1,20 @@
 package com.bagal.factories;
 
+import com.bagal.configs.ConfigReader;
+import com.bagal.driver.brwoseroptions.ChromeBrowserOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public final class WebDriverFactory {
     private WebDriverFactory() {}
     public static WebDriver getWebDriver() {
-        return new ChromeDriver();
+        WebDriver driver;
+        if (ConfigReader.getConfig().browser().equalsIgnoreCase("firefox")) {
+            driver= new FirefoxDriver();
+        }else{
+            driver= new ChromeDriver(ChromeBrowserOptions.getChromeBrowserOptions());
+        }
+        return driver;
     }
 }
