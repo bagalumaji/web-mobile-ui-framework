c045f45e104d3580ae44151638ae4a35b2e98279
umaji <bagalumaji@gmail.com>
2024-10-02 09:54:57 +0530

    added impln of Browser Not support exception

--- src/main/java/com/bagal/exceptions/BrowserNotSupportedException.java
+++ src/main/java/com/bagal/exceptions/BrowserNotSupportedException.java
@@ -1,4 +1,7 @@
 package com.bagal.exceptions;
 
 public class BrowserNotSupportedException extends RuntimeException {
+    public BrowserNotSupportedException(String message) {
+        super(message);
+    }
 }
