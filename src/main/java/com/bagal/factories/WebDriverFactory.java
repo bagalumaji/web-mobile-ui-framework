25f0343d684f1d1af8ba5630be94b1b50a6ffe3b
umaji <bagalumaji@gmail.com>
2024-10-02 09:55:43 +0530

    updated web driver factory

--- src/main/java/com/bagal/factories/WebDriverFactory.java
+++ src/main/java/com/bagal/factories/WebDriverFactory.java
@@ -2,6 +2,7 @@ package com.bagal.factories;
 
 import com.bagal.configs.ConfigReader;
 import com.bagal.driver.brwoseroptions.ChromeBrowserOptions;
+import com.bagal.exceptions.BrowserNotSupportedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,10 +11,15 @@ public final class WebDriverFactory {
     private WebDriverFactory() {}
     public static WebDriver getWebDriver() {
         WebDriver driver;
-        if (ConfigReader.getConfig().browser().equalsIgnoreCase("firefox")) {
-            driver= new FirefoxDriver();
-        }else{
-            driver= new ChromeDriver(ChromeBrowserOptions.getChromeBrowserOptions());
+        switch (ConfigReader.getConfig().browser()) {
+            case FIREFOX:
+                driver = new FirefoxDriver();
+                break;
+            case CHROME:
+                driver = new ChromeDriver(ChromeBrowserOptions.getChromeBrowserOptions());
+                break;
+            default:
+                throw new BrowserNotSupportedException(ConfigReader.getConfig().browser().name()+" browser is not supported");
         }
         return driver;
     }
