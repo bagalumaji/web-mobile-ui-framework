6c0fc5d964b1027d63fe06e97e2f9139bf31c6b9
umaji <bagalumaji@gmail.com>
2024-10-03 07:58:35 +0530

    added BrowserStackFactory

---
+++ src/main/java/com/bagal/driver/web/remote/BrowserStackFactory.java
@@ -0,0 +1,13 @@
+package com.bagal.driver.web.remote;
+
+import com.bagal.driver.manager.web.remote.browserstack.BrowserStackChromeManager;
+import com.bagal.driver.manager.web.remote.browserstack.BrowserStackFirefoxManager;
+import com.bagal.enums.WebBrowser;
+import org.openqa.selenium.WebDriver;
+
+public final class BrowserStackFactory {
+    private BrowserStackFactory() {}
+    public static WebDriver getDriver(WebBrowser browser) {
+        return browser==WebBrowser.CHROME? BrowserStackChromeManager.getDriver(): BrowserStackFirefoxManager.getDriver();
+    }
+}
