677feb694ed82f964d151b0997c6e5bdde1d5960
umaji <bagalumaji@gmail.com>
2024-10-03 08:01:32 +0530

    updated RemoteDriverFactory

---
+++ src/main/java/com/bagal/driver/web/remote/RemoteDriverFactory.java
@@ -0,0 +1,22 @@
+package com.bagal.driver.web.remote;
+
+import com.bagal.enums.RemoteService;
+import com.bagal.enums.WebBrowser;
+import org.openqa.selenium.WebDriver;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public final class RemoteDriverFactory {
+    private RemoteDriverFactory() {}
+
+    private static final Map<RemoteService, Function<WebBrowser,WebDriver>> MAP= new EnumMap<>(RemoteService.class);
+    static {
+        MAP.put(RemoteService.BROWSER_STACK,BrowserStackFactory::getDriver);
+        MAP.put(RemoteService.LAMBDA_TEST,LambdaTestFactory::getDriver);
+    }
+    public static WebDriver getDriver(RemoteService remoteService, WebBrowser webBrowser) {
+        return MAP.get(remoteService).apply(webBrowser);
+    }
+}
