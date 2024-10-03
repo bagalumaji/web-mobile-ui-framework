872a47cfc0a3a6c2ff5678f3e4b46d3908093277
umaji <bagalumaji@gmail.com>
2024-10-03 08:02:48 +0530

    updated test

--- src/test/java/com/bagal/tests/web/WebDemoTest.java
+++ src/test/java/com/bagal/tests/web/WebDemoTest.java
@@ -1,10 +1,21 @@
 package com.bagal.tests.web;
 
+import com.bagal.configs.BrowserStackConfigReader;
+import com.bagal.configs.ConfigReader;
 import com.bagal.driver.manager.DriverManager;
 import com.bagal.testsetup.WebTestSetup;
 import org.testng.annotations.Test;
 
 public class WebDemoTest extends WebTestSetup {
+    public static void main(String[] args) {
+        System.out.println("ConfigReader.getConfig().executionMode() = " + ConfigReader.getConfig().executionMode());
+        System.out.println("ConfigReader.getConfig().executionPlatform() = " + ConfigReader.getConfig().executionPlatform());
+        System.out.println("ConfigReader.getConfig().remoteService() = " + ConfigReader.getConfig().remoteService());
+        System.out.println("ConfigReader.getConfig().browser() = " + ConfigReader.getConfig().webBrowser());
+        System.out.println("ConfigReader.getConfig().mobilePlatform() = " + ConfigReader.getConfig().mobilePlatform());
+
+        System.out.println("BrowserStackConfigReader.getConfig().browserStackUrl() = " + BrowserStackConfigReader.getConfig().browserStackUrl());
+    }
     @Test
     public void demoTest(){
         DriverManager.getWebDriver().get("https://the-internet.herokuapp.com/login");
