3995e4d793adc81e76cac4c2fad7484510388285
umaji <bagalumaji@gmail.com>
2024-10-04 21:36:19 +0530

    added LambdaTestMobileFactory

---
+++ src/main/java/com/bagal/driver/factory/mobile/remote/LambdaTestMobileFactory.java
@@ -0,0 +1,10 @@
+package com.bagal.driver.factory.mobile.remote;
+
+import com.bagal.enums.MobilePlatform;
+import org.openqa.selenium.WebDriver;
+
+public class LambdaTestMobileFactory {
+    public static WebDriver getDriver(MobilePlatform mobilePlatform) {
+        return null;
+    }
+}
