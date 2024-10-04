8f1e40f7280f38b5405ab97ce277267eeffd3bbf
umaji <bagalumaji@gmail.com>
2024-10-04 21:25:59 +0530

    added BrowserStackAndroidManager for remote android

---
+++ src/main/java/com/bagal/driver/manager/mobile/remote/browserstack/BrowserStackAndroidManager.java
@@ -0,0 +1,9 @@
+package com.bagal.driver.manager.mobile.remote.browserstack;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserStackAndroidManager {
+    public static WebDriver getDriver() {
+        return null;
+    }
+}
