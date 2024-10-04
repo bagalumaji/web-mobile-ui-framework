00d25694c9f3bd8a74be49ca378e3685dc58bb78
umaji <bagalumaji@gmail.com>
2024-10-04 21:29:55 +0530

    added BrowserStackIosManager

---
+++ src/main/java/com/bagal/driver/manager/mobile/remote/browserstack/BrowserStackIosManager.java
@@ -0,0 +1,9 @@
+package com.bagal.driver.manager.mobile.remote.browserstack;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserStackIosManager {
+    public static WebDriver getDriver() {
+        return null;
+    }
+}
