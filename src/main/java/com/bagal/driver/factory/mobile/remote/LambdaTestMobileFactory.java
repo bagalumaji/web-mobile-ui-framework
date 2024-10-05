404c9f8a000fd3c49ae3761ad008b9f274b19140
umaji <bagalumaji@gmail.com>
2024-10-05 22:16:18 +0530

    updated

--- src/main/java/com/bagal/driver/factory/mobile/remote/LambdaTestMobileFactory.java
+++ src/main/java/com/bagal/driver/factory/mobile/remote/LambdaTestMobileFactory.java
@@ -1,10 +1,21 @@
 package com.bagal.driver.factory.mobile.remote;
 
+import com.bagal.driver.manager.mobile.remote.lambdatest.LambdaTestAndroidManager;
+import com.bagal.driver.manager.mobile.remote.lambdatest.LambdaTestIosManager;
 import com.bagal.enums.MobilePlatform;
 import org.openqa.selenium.WebDriver;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class LambdaTestMobileFactory {
+    private static final Map<MobilePlatform, Supplier<WebDriver>> MAP = new EnumMap<>(MobilePlatform.class);
+    static {
+        MAP.put(MobilePlatform.ANDROID, LambdaTestAndroidManager::getDriver);
+        MAP.put(MobilePlatform.IOS, LambdaTestIosManager::getDriver);
+    }
     public static WebDriver getDriver(MobilePlatform mobilePlatform) {
-        return null;
+        return MAP.get(mobilePlatform).get();
     }
 }
