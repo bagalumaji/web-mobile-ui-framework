b198e88c57db70476d9eda10975f6da18dd85be2
umaji bagal <bagalumaji@gmail.com>
2024-10-08 10:03:27 +0530

    updated

--- src/main/java/com/bagal/driver/manager/mobile/local/LocalIosManager.java
+++ src/main/java/com/bagal/driver/manager/mobile/local/LocalIosManager.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 public final class LocalIosManager {
     private LocalIosManager() {}
     public static WebDriver getDriver(){
+        XCUITestOptions options = new XCUITestOptions();
+        options.setApp(System.getProperty("user.dir")+"/src/test/resources/apps/SauceLabs-Demo-App.Simulator.XCUITest.zip");
         return new IOSDriver(ConfigReader.getConfig().localAppiumServerUrl(),new XCUITestOptions());
     }
 }
