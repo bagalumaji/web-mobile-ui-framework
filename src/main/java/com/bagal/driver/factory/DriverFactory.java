caf3ce19140d318820060660124a7632df72844f
umaji bagal <bagalumaji@gmail.com>
2024-10-09 00:47:39 +0530

    updated method name

--- src/main/java/com/bagal/driver/factory/DriverFactory.java
+++ src/main/java/com/bagal/driver/factory/DriverFactory.java
@@ -6,22 +6,11 @@ import com.bagal.driver.factory.web.local.LocalDriverFactory;
 import com.bagal.driver.factory.web.remote.RemoteDriverFactory;
 import com.bagal.entity.DriverData;
 import com.bagal.enums.ExecutionMode;
-import com.bagal.enums.RemoteService;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public final class DriverFactory {
     private DriverFactory() {
     }
-
-    //    private static final Map<ExecutionMode, Function<WebBrowser,WebDriver>> LOCAL_FACTORY = new EnumMap<>(ExecutionMode.class);
-//    private static final Map<ExecutionMode, BiFunction<ExecutionPlatform, MobilePlatform, WebDriver>> REMOTE_FACTORY = new EnumMap<>(ExecutionMode.class);
-//
-//
-//    static {
-//       WEB_FACTORY.put(ExecutionMode.LOCAL,LocalDriverFactory::getWebDriver);
-//       WEB_FACTORY.put(ExecutionMode.REMOTE, RemoteDriverFactory::getDriver);
-//    }
 
     public static WebDriver getWebDriver(DriverData driverData) {
         if(driverData.getExecutionMode()== ExecutionMode.LOCAL){
