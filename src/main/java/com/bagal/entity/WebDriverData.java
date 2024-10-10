830ed59b1f83f78abbdd16a7dabba4986892983a
umaji bagal <bagalumaji@gmail.com>
2024-10-10 20:54:42 +0530

    created WebDriverData

---
+++ src/main/java/com/bagal/entity/WebDriverData.java
@@ -0,0 +1,14 @@
+package com.bagal.entity;
+
+import com.bagal.enums.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class WebDriverData {
+    WebBrowser webBrowser;
+    ExecutionMode executionMode;
+    ExecutionPlatform executionPlatform;
+    RemoteService remoteService;
+}
