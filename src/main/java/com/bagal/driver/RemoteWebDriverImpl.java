2b1983bb1a3338eee83cdd8d7e8ce3e032f75fbe
umaji bagal <bagalumaji@gmail.com>
2024-10-10 20:54:30 +0530

    created RemoteWebDriverImpl

---
+++ src/main/java/com/bagal/driver/RemoteWebDriverImpl.java
@@ -0,0 +1,12 @@
+package com.bagal.driver;
+
+import com.bagal.driver.factory.web.remote.RemoteDriverFactory;
+import com.bagal.entity.WebDriverData;
+import org.openqa.selenium.WebDriver;
+
+public class RemoteWebDriverImpl implements IWebDriver{
+    @Override
+    public WebDriver getDriver(WebDriverData webDriverData) {
+        return RemoteDriverFactory.getDriver(webDriverData.getRemoteService(),webDriverData.getWebBrowser());
+    }
+}
