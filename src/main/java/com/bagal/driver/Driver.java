71ee7ff56fd197a0af2378b999d9f280c7317e1f
umaji bagal <bagalumaji@gmail.com>
2024-10-10 20:53:16 +0530

    driver updated

--- src/main/java/com/bagal/driver/Driver.java
+++ src/main/java/com/bagal/driver/Driver.java
@@ -2,7 +2,8 @@ package com.bagal.driver;
 
 import com.bagal.configs.ConfigReader;
 import com.bagal.driver.factory.DriverFactory;
-import com.bagal.entity.DriverData;
+import com.bagal.entity.MobileDriverData;
+import com.bagal.entity.WebDriverData;
 import org.openqa.selenium.WebDriver;
 
 public final class Driver {
@@ -16,22 +17,22 @@ public final class Driver {
     }
 
     public static void intializeWebDriver() {
-        DriverData driverData=DriverData.builder()
+        WebDriverData webDriverData = WebDriverData.builder()
                 .executionMode(ConfigReader.getConfig().executionMode())
                 .executionPlatform(ConfigReader.getConfig().executionPlatform())
                 .remoteService(ConfigReader.getConfig().remoteService())
                 .webBrowser(ConfigReader.getConfig().webBrowser())
                 .build();
-       WebDriver driver= DriverFactory.getWebDriver(driverData);
+       WebDriver driver= DriverFactory.getWebDriver(webDriverData);
 
     }
     public static void intializeMobileDriver() {
-        DriverData driverData= DriverData.builder()
+        MobileDriverData mobileDriverData = MobileDriverData.builder()
                 .executionMode(ConfigReader.getConfig().executionMode())
                 .executionPlatform(ConfigReader.getConfig().executionPlatform())
                 .remoteService(ConfigReader.getConfig().remoteService())
                 .mobilePlatform(ConfigReader.getConfig().mobilePlatform())
                 .build();
-        WebDriver driver= DriverFactory.getMobileDriver(driverData);
+        WebDriver driver= DriverFactory.getMobileDriver(mobileDriverData);
     }
 }
