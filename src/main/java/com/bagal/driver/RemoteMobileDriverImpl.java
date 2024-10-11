88adc34cd0995f76dbb49e5cd9cdf51d2dd8fa53
umaji bagal <bagalumaji@gmail.com>
2024-10-11 21:27:09 +0530

    created RemoteMobileDriverImpl

---
+++ src/main/java/com/bagal/driver/RemoteMobileDriverImpl.java
@@ -0,0 +1,12 @@
+package com.bagal.driver;
+
+import com.bagal.driver.factory.mobile.remote.RemoteMobileDriverFactory;
+import com.bagal.entity.MobileDriverData;
+import org.openqa.selenium.WebDriver;
+
+public final class RemoteMobileDriverImpl implements IMobileDriver{
+    @Override
+    public WebDriver getDriver(final MobileDriverData mobileDriverData) {
+        return RemoteMobileDriverFactory.getDriver(mobileDriverData.getRemoteService(),mobileDriverData.getMobilePlatform());
+    }
+}
