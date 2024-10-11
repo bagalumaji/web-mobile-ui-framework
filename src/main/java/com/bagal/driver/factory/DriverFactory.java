5f7acc8c1fc68bf57489992eb933bf6b29c506a4
umaji bagal <bagalumaji@gmail.com>
2024-10-11 22:03:55 +0530

    updated with supplier interface

--- src/main/java/com/bagal/driver/factory/DriverFactory.java
+++ src/main/java/com/bagal/driver/factory/DriverFactory.java
@@ -1,29 +1,40 @@
 package com.bagal.driver.factory;
 
-import com.bagal.driver.LocalMobileDriverImpl;
-import com.bagal.driver.LocalWebDriverImpl;
-import com.bagal.driver.RemoteMobileDriverImpl;
-import com.bagal.driver.RemoteWebDriverImpl;
+import com.bagal.driver.*;
 import com.bagal.entity.MobileDriverData;
 import com.bagal.entity.WebDriverData;
 import com.bagal.enums.ExecutionMode;
 import org.openqa.selenium.WebDriver;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public final class DriverFactory {
     private DriverFactory() {
     }
+    private static final Map<ExecutionMode, Supplier<IWebDriver>> WEB_DRIVER = new EnumMap<>(ExecutionMode.class);
+    private static final Map<ExecutionMode, Supplier<IMobileDriver>> MOBILE_DRIVER = new EnumMap<>(ExecutionMode.class);
 
+static {
+    WEB_DRIVER.put(ExecutionMode.LOCAL,LocalWebDriverImpl::new);
+    WEB_DRIVER.put(ExecutionMode.REMOTE,RemoteWebDriverImpl::new);
+    MOBILE_DRIVER.put(ExecutionMode.REMOTE,RemoteMobileDriverImpl::new);
+    MOBILE_DRIVER.put(ExecutionMode.LOCAL,LocalMobileDriverImpl::new);
+}
     public static WebDriver getWebDriver(WebDriverData webDriverData) {
-        return webDriverData.getExecutionMode() == ExecutionMode.LOCAL
+    return WEB_DRIVER.get(webDriverData.getExecutionMode()).get().getDriver(webDriverData);
+        /*return webDriverData.getExecutionMode() == ExecutionMode.LOCAL
                 ? new LocalWebDriverImpl().getDriver(webDriverData)
-                : new RemoteWebDriverImpl().getDriver(webDriverData);
+                : new RemoteWebDriverImpl().getDriver(webDriverData);*/
     }
 
 
 
     public static WebDriver getMobileDriver(MobileDriverData mobileDriverData) {
-        return mobileDriverData.getExecutionMode() == ExecutionMode.LOCAL
+    return MOBILE_DRIVER.get(mobileDriverData.getExecutionMode()).get().getDriver(mobileDriverData);
+        /*return mobileDriverData.getExecutionMode() == ExecutionMode.LOCAL
                 ? new LocalMobileDriverImpl().getDriver(mobileDriverData)
-                : new RemoteMobileDriverImpl().getDriver(mobileDriverData);
+                : new RemoteMobileDriverImpl().getDriver(mobileDriverData);*/
     }
 }
