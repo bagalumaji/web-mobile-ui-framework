6c5560a512758be09fa13530dc54f09eb83b31c4
umaji bagal <bagalumaji@gmail.com>
2024-10-12 10:20:37 +0530

    added context code

--- src/main/java/com/bagal/driver/manager/DriverManager.java
+++ src/main/java/com/bagal/driver/manager/DriverManager.java
@@ -1,22 +1,45 @@
 package com.bagal.driver.manager;
 
+import com.bagal.enums.ExecutionPlatform;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.WebDriver;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 public final class DriverManager {
     private DriverManager() {
     }
 
     private final static ThreadLocal<WebDriver> WEB_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+    private final static ThreadLocal<WebDriver> MOBILE_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+    private final static ThreadLocal<ExecutionPlatform> CONTEXT = new ThreadLocal<>();
+    private final static Map<ExecutionPlatform,ThreadLocal<WebDriver>> EXECUTION_PLATFORM_THREAD_LOCAL = new EnumMap<>(ExecutionPlatform.class);
 
-    public static WebDriver getWebDriver() {
-        return WEB_DRIVER_THREAD_LOCAL.get();
+
+    public static WebDriver getDriver() {
+        return CONTEXT.get()==ExecutionPlatform.WEB?WEB_DRIVER_THREAD_LOCAL.get():MOBILE_DRIVER_THREAD_LOCAL.get();
     }
 
-    public static void setWebDriver(WebDriver driver) {
-        WEB_DRIVER_THREAD_LOCAL.set(driver);
+    public static void setDriver(WebDriver driver) {
+        if (isMobileDriver(driver)) {
+            MOBILE_DRIVER_THREAD_LOCAL.set(driver);
+            EXECUTION_PLATFORM_THREAD_LOCAL.put(ExecutionPlatform.MOBILE,MOBILE_DRIVER_THREAD_LOCAL);
+            CONTEXT.set(ExecutionPlatform.MOBILE);
+        } else {
+            WEB_DRIVER_THREAD_LOCAL.set(driver);
+            EXECUTION_PLATFORM_THREAD_LOCAL.put(ExecutionPlatform.WEB,WEB_DRIVER_THREAD_LOCAL);
+            CONTEXT.set(ExecutionPlatform.WEB);
+        }
     }
 
+    private static boolean isMobileDriver(WebDriver driver) {
+        return driver instanceof AndroidDriver || driver instanceof IOSDriver;
+    }
     public static void unload() {
         WEB_DRIVER_THREAD_LOCAL.remove();
+        MOBILE_DRIVER_THREAD_LOCAL.remove();
+        CONTEXT.remove();
     }
 }
