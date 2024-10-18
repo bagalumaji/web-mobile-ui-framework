b424129e5571f45a3edc95362cdf83880b6a7c37
umaji bagal <bagalumaji@gmail.com>
2024-10-18 09:22:36 +0530

    added methods

--- src/main/java/com/bagal/actions/web/PageActions.java
+++ src/main/java/com/bagal/actions/web/PageActions.java
@@ -3,4 +3,6 @@ package com.bagal.actions.web;
 public final class PageActions {
     private PageActions() {}
     public static void navigateTo(String url) {}
+    public static void enterText(String text){}
+    public static void click(){}
 }
