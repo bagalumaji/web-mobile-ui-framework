df4bbc3b64cb5d725e1841544fc09f855b5754c6
umaji bagal <bagalumaji@gmail.com>
2024-10-21 09:49:44 +0530

    added wait methods

--- src/main/java/com/bagal/factories/ExplicitWaitFactory.java
+++ src/main/java/com/bagal/factories/ExplicitWaitFactory.java
@@ -1,5 +1,11 @@
 package com.bagal.factories;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public final class ExplicitWaitFactory {
     private ExplicitWaitFactory() {}
+    public static void waiForElementToVisible(WebElement element) {}
+    public static void waiForElementToVisible(By locator) {}
+
 }
