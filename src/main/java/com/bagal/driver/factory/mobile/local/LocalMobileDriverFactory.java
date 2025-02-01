294ed261a0601956cb3be793a4daa85bf399f466
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-02-01 10:02:12 +0530

    updated with exception if not present

--- src/main/java/com/bagal/driver/factory/mobile/local/LocalMobileDriverFactory.java
+++ src/main/java/com/bagal/driver/factory/mobile/local/LocalMobileDriverFactory.java
@@ -7,16 +7,22 @@ import org.openqa.selenium.WebDriver;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public final class LocalMobileDriverFactory {
     private LocalMobileDriverFactory() {}
-    private static final Map<MobilePlatform, Supplier<WebDriver>> MAP=new EnumMap<>(MobilePlatform.class);
+
+    private static final Map<MobilePlatform, Supplier<WebDriver>> MAP = new EnumMap<>(MobilePlatform.class);
+
     static {
         MAP.put(MobilePlatform.ANDROID, LocalAndroidManager::getDriver);
         MAP.put(MobilePlatform.IOS, LocalIosManager::getDriver);
     }
+
     public static WebDriver getDriver(MobilePlatform platform) {
-        return MAP.get(platform).get();
+        return Optional.ofNullable(MAP.get(platform))
+                .orElseThrow(() -> new IllegalArgumentException("No driver found for platform: " + platform))
+                .get();
     }
 }
