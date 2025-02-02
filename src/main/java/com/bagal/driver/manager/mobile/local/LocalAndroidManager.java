88fbbe87864a5233799f5162ae9e1f4f1fdac2fc
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-02-02 20:05:03 +0530

    updated with single session

--- src/main/java/com/bagal/driver/manager/mobile/local/LocalAndroidManager.java
+++ src/main/java/com/bagal/driver/manager/mobile/local/LocalAndroidManager.java
@@ -1,11 +1,9 @@
 package com.bagal.driver.manager.mobile.local;
 
-import com.bagal.utils.PortUtils;
+import com.bagal.configs.ConfigReader;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.WebDriver;
-
-import java.net.URL;
 
 public final class LocalAndroidManager {
     private LocalAndroidManager() {
@@ -13,22 +11,7 @@ public final class LocalAndroidManager {
 
     public static WebDriver getDriver() {
         UiAutomator2Options options = new UiAutomator2Options();
-        int port = PortUtils.getAvailablePort();
-        if (port == 4723) {
-            options.setUdid("emulator-5554");
-        }
-        if (port == 4724) {
-            options.setUdid("emulator-5556");
-        }
         options.setApp(System.getProperty("user.dir") + "/src/test/resources/apps/mda-2.1.0-24.apk");
-        return new AndroidDriver(getUrl(port), options);
-    }
-
-    private static URL getUrl(int port) {
-        try {
-            return new URL("http://127.0.0.1:" + port);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return new AndroidDriver(ConfigReader.getConfig().localAppiumServerUrl(), options);
     }
 }
